a341d8837fa976a30d83e1ee3e211859fb33a106
Momin Ali <mominali4015@gmail.com>
2022-12-01 12:26:54 +0500

    work on webDev page

--- resources/views/pages/web-development.blade.php
+++ resources/views/pages/web-development.blade.php
@@ -6,10 +6,8 @@
             <h4 style="padding-top: 50px;">Best Web Design and Development Service</h4>
         </div>
     </div>
-
-
-    <div class="container-fluid " style="padding-top: 40px;">
-        <div class="row for-mobile">
+    <div class="container-fluid ">
+        <div class="row for-mobile"  style="padding-top: 40px;">
             <div class="col-md-1"></div>
             <div class="col-md-10">
                 <h4 style="padding-bottom: 20px;">Code For The Next Level Development</h4>
@@ -25,6 +23,8 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container-fluid">
         <div class="row for-mobile" style="padding-top: 40px;">
             <div class="col-md-1"></div>
             <div class="col-md-10">
@@ -44,51 +44,199 @@
                     their competitive online presence.</p>
             </div>
             <div class="col-md-1"></div>
-
         </div>
-
-        <div class="container-fluid">
-            <div class="row for-mobile">
-                <div class="col-md-1"></div>
-                <div class="col-md-7">
-                    <h4 style="padding-top: 30px; padding-bottom:20px ">Content Management Syste</h4>
-                    <p class="text-justify">Although maintaining a content management system (CMS) is thought to be
-                        challenging, we're making it incredibly simple by using our own methods. Here, thanks to the
-                        work of our web developers,
-                        our clients can review and edit the content that has been created.
-                        You can get in touch with us through CMS web development.</p>
+    </div>
+    <div class="container-fluid">
+        <div class="row for-mobile">
+            <div class="col-md-1"></div>
+            <div class="col-md-7">
+                <h4 style="padding-top: 30px; padding-bottom:20px ">Content Management Syste</h4>
+                <p class="text-justify">Although maintaining a content management system (CMS) is thought to be
+                    challenging, we're making it incredibly simple by using our own methods. Here, thanks to the
+                    work of our web developers,
+                    our clients can review and edit the content that has been created.
+                    You can get in touch with us through CMS web development.</p>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+                <div style="height: 70%;width: 70%;padding-top: 30px;">
+                    <img src="{{ asset('Images/cms.png') }}" class="img-fluid">
                 </div>
-                <div class="col-md-1"></div>
-                <div class="col-md-3">
-                    <div style="height: 70%;width: 70%;padding-top: 30px;">
-                        <img src="{{ asset('Images/cms.png') }}" class="img-fluid">
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row for-mobile">
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+                <div style="height: 70%;width: 70%;padding-top: 30px;">
+                    <img src="{{ asset('Images/crm_pic.png') }}" class="img-fluid">
+                </div>
+            </div>
+            <div class="col-md-7 text-justify">
+                <h4 style="padding-top: 30px; padding-bottom: 20px">Customer Relationship Management (CRM)
+                </h4>
+                <p>A customer relationship management (CRM) tool enables you to manage your business contacts
+                    more
+                    effectively. It provides the opportunity to utilise a variety of marketing strategies in
+                    order to
+                    draw in and cultivate brand loyalty from customers. Right now, maintaining a special
+                    relationship
+                    with customers is a great factor and a guarantee of a company's reputation. At Techsters, we
+                    set up
+                    a CRM based on your activity by identifying the essential indicators to consider.
+                    Processing and analysing customer data will enable you to define good business planning.</p>
+            </div>
+            <div class="col-md-1"></div>
+        </div>
+        <div class="row for-mobile">
+            <div class="col-md-4 col-sm-4"></div>
+            <div class="col-md-8 col-sm-6">
+                <h5 style="padding-top: 10px; padding-bottom: 20px">Our CRM expertise</h5>
+                <ul>
+                    <li >Identification of Customer Needs
+                    </li>
+                    <li >After Sales</li>
+                    <li >Assessment KPI's</li>
+                    <li >Customer Relationship</li>
+                    <li >Improved Sales</li>
+                    <li >Prospect Contact Base</li>
+                    <li >Customer Follow-up</li>
+                    <li >Customer Follow-up</li>
+                    <li >Marketing</li>
+                </ul>
+            </div>
+            <div class="col-md-2 col-sm-2"></div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row for-mobile">
+            <div class="col-md-1"></div>
+            <div class="col-md-7">
+                <h4 style="padding-top: 30px; padding-bottom:20px ">Wordpress</h4>
+                <p class="text-justify">If you’re looking for Wordpress, trust us that we’ve
+                    got it covered for you. In case you’re having any sort of inconvenience in your project or
+                    assignment, our web developers can make this job really easy for you.
+                    We’re a web developing company who is bringing much needed
+                    advancement in wordpress customization and we’re doing it in style.</p>
+            </div>
+            <div class="col-md-1"></div>
+            <div class="col-md-3">
+                <div style="height: 70%;width: 70%;padding-top: 30px;">
+                    <img src="{{ asset('Images/cms.png') }}" class="img-fluid">
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid text-justify"style="padding-bottom: 20px;">
+        <div class="row for-mobile d-flex justify-content-evenly">
+            <div class=" col-lg-3 col-md-4  my-3">
+                <div class="card" style="height: 380px;">
+                    <div class="card-body">
+                        <div class="row ">
+                            <div class="col-md-12 justify-content-center d-flex">
+                                <div style="height: 40%;width: 70%;">
+                                    <img src="{{ asset('Images/gif/website.gif') }}" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <h5 class="card-title text-center">Corporate Website</h5>
+                        <p class="card-text card1-text">Web-based platform for document management, personnel management
+                            and information exchange between
+                            branches and remote units. Requires exclusive design and 24/7 support</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 my-3">
+                <div class="card" style="height: 380px;">
+                    <div class="card-body">
+                        <div class="row ">
+                            <div class="col-md-12 justify-content-center d-flex">
+                                <div style="height: 40%;width: 70%;">
+                                    <img src="{{ asset('Images/gif/landingsite.gif') }}" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <h5 class="card-title text-center">Landing Site</h5>
+                        <p class="card-text card1-text">A one-page website to inform potential buyers about the benefits of the product.
+                            An autonomous advertising resource requiring a catchy design and quality content.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4 my-3">
+                <div class="card" style="height: 380px;">
+                    <div class="card-body">
+                        <div class="row ">
+                            <div class="col-md-12 justify-content-center d-flex">
+                                <div style="height: 40%;width: 70%;">
+                                    <img src="{{ asset('Images/gif/portal.gif') }}" class="img-fluid ">
+                                </div>
+                            </div>
+                        </div>
+                        <h5 class="card-title text-center">Portal</h5>
+                        <p class="card-text card1-text">A large Internet resource with a large amount of information. Optimal for publishing news articles, sports reports, announcements.
+                            Attracts a lot of visitors and provides passive income by placing ads on the most visited pages.</p>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="container-fluid">
-            <div class="row for-mobile">
-                <div class="col-md-1"></div>
-                <div class="col-md-3">
-                    <div style="height: 70%;width: 70%;padding-top: 30px;">
-                        <img src="{{ asset('Images/crm_pic.png') }}" class="img-fluid">
+    </div>
+    <div class="container-fluid text-justify"style="padding-bottom: 20px;">
+        <div class="row for-mobile d-flex justify-content-evenly">
+            <div class="col-lg-3 col-md-4 my-3">
+                <div class="card" style="height: 380px;">
+                    <div class="card-body">
+                        <div class="row ">
+                            <div class="col-md-12 justify-content-center d-flex">
+                                <div style="height: 40%;width: 70%;">
+                                    <img src="{{ asset('Images/gif/responsivesite.gif') }}" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <h5 class="card-title text-center">Responsive Sites</h5>
+                        <p class="card-text card1-text">A resource that provides the correct display of information on any devices (smartphones, tablets).
+                            Attracts mobile users, increases business competitiveness.</p>
                     </div>
                 </div>
-                <div class="col-md-7 text-justify">
-                    <h4 style="padding-top: 30px; padding-bottom: 20px">Customer Relationship Management (CRM)
-                    </h4>
-                    <p>A customer relationship management (CRM) tool enables you to manage your business contacts
-                        more
-                        effectively. It provides the opportunity to utilise a variety of marketing strategies in
-                        order to
-                        draw in and cultivate brand loyalty from customers. Right now, maintaining a special
-                        relationship
-                        with customers is a great factor and a guarantee of a company's reputation. At Techsters, we
-                        set up
-                        a CRM based on your activity by identifying the essential indicators to consider.
-                        Processing and analysing customer data will enable you to define good business planning.</p>
+            </div>
+            <div class="col-lg-3 col-md-4 my-3">
+                <div class="card" style="height: 380px;">
+                    <div class="card-body">
+                        <div class="row ">
+                            <div class="col-md-12 justify-content-center d-flex">
+                                <div style="height: 40%;width: 70%;">
+                                    <img src="{{ asset('Images/gif/portal.gif') }}" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <h5 class="card-title text-center">E-Commerce</h5>
+                        <p class="card-text card1-text">A one-page website to inform potential buyers about the benefits of the product.
+                            An autonomous advertising resource requiring a catchy design and quality content.</p>
+                    </div>
                 </div>
-                <div class="col-md-1"></div>
+            </div>
+            <div class="col-lg-3 col-md-4 my-3">
+                <div class="card" style="height: 380px;">
+                    <div class="card-body">
+                        <div class="row ">
+                            <div class="col-md-12 justify-content-center d-flex">
+                                <div style="height: 40%;width: 70%;">
+                                    <img src="{{ asset('Images/gif/portal.gif') }}" class="img-fluid">
+                                </div>
+                            </div>
+                        </div>
+                        <h5 class="card-title text-center">Portfolio</h5>
+                        <p class="card-text card1-text">A large Internet resource with a large amount of information. Optimal for publishing news articles, sports reports, announcements.
+                            Attracts a lot of visitors and provides passive income by placing ads on the most visited pages.</p>
+                    </div>
+                </div>
             </div>
         </div>
+    </div>
+
 @endsection
+
+
+
+
+
